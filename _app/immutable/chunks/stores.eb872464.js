7d9b19ae6d387739abad9cd8a21af895e68d130d
Prashant Gajavelli <gajavelli@gmail.com>
2023-06-20 23:27:27 -0400

    Add files via upload

---
+++ _app/immutable/chunks/stores.eb872464.js
@@ -0,0 +1,1 @@
+import{s as e}from"./singletons.5f8c2d86.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
